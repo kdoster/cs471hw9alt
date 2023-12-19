6cdfe8e3d686499abda0609a304c1657a8d893bd
Kyle Doster <kyledoster@u.boisestate.edu>
2023-12-18 20:30:35 -0700

    Step 9

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -34,6 +34,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
 
+import java.util.Random;
+
 @Controller
 @SpringBootApplication
 public class HerokuApplication {
@@ -57,9 +59,9 @@ public class HerokuApplication {
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
+      stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
+      ResultSet rs = stmt.executeQuery("SELECT tick, random_string FROM table_timestamp_and_random_string");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
@@ -83,6 +85,16 @@ public class HerokuApplication {
       config.setJdbcUrl(dbUrl);
       return new HikariDataSource(config);
     }
+  }
+
+  public String getRandomString(){
+    Random random = new Random();
+    String characters = "abcdefghijklmnopqrstuvwxyz";
+    StringBuilder sb = new StringBuilder(12);
+    for(int i = 0; i<12; i++){
+      sb.append(characters.charAt(random.nextInt(characters.length())));
+    }
+    return sb.toString();
   }
 
 }
