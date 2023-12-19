dd5ae1a839bb985ac91bd5ac3f0094fe15beace1
Kyle Doster <kyledoster@u.boisestate.edu>
2023-12-18 20:50:59 -0700

    Step 10

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -65,10 +65,11 @@ public class HerokuApplication {
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick") + rs.getString("random_string"));
+        output.add("Read from DB: " + rs.getTimestamp("tick") + " " + rs.getString("random_string"));
       }
 
       model.put("records", output);
+      System.out.println("Kyle Doster");
       return "db";
     } catch (Exception e) {
       model.put("message", e.getMessage());
