c0eace98475948f6e63f2b2d6c094976c25f1c00
Kyle Doster <kyledoster@u.boisestate.edu>
2023-12-18 20:40:21 -0700

    Step 9 with better output

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -65,7 +65,7 @@ public class HerokuApplication {
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
+        output.add("Read from DB: " + rs.getTimestamp("tick") + rs.getString("random_string"));
       }
 
       model.put("records", output);
